465e34dc82c7c5b931d473269f43401179102032
mamingjian <252988605@qq.com>
2019-03-06 07:54:09 +0000

    Finish user edit, update, index, and destroy actions

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::get('/signup', 'UsersController@create')->name('signup');
 
 //resource
 Route::resource('users', 'UsersController');
+Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
 
 //login
 Route::get('login', 'SessionsController@create')->name('login');
