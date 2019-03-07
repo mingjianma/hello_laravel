f20e90bb12aaf2d8d96e36cb89196e4cd7064817
mamingjian <252988605@qq.com>
2019-03-07 03:27:28 +0000

    Add password resets & email configuration

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,9 +17,10 @@ class UsersTableSeeder extends Seeder
 
         $user = User::find(1);
         $user->name = 'Aufree';
-        $user->is_admin = true;
         $user->email = 'aufree@yousails.com';
         $user->password = bcrypt('password');
+        $user->is_admin = true;
+        $user->activated = true;
         $user->save();
     }
 }
